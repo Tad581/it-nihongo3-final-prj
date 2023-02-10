f3069c84a8360fd1772c063163ab467e4dd90363
tadd581 <Kimbaoksqp123>
2023-02-10 13:14:19 +0700

    setup room

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
+  # resources :follows
+  # resources :posts
+  resources :rooms
+  # do
+  #   resources :messages
+  # end
   root "pages#home"
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    get "users", to: "devise/session#new"
-  end
+  devise_for :users, controllers: {
+                       sesions: "users/sessions",
+                       registrations: "users/registrations",
+                     }
+  get "user/:id", to: "users#show", as: "user"
+  # get "/search", to: "pages#search"
+  # get "/request_list", to: "follows#request"
+  # get "/send_friend_request", to: "follows#send_friend_request"
+  # get "/accept_friend_request", to: "follows#send_friend_request"
+  # get "/delete_friend_request", to: "follows#delete_friend_request"
+  # get "/cancel_friend_request", to: "follows#cancel_friend_request"
+  # get "/profile", to: "users#profile"
   # Defines the root path route ("/")
   # root "articles#index"
 end
