cdc56443f3f6617555484374e0b9107ddb3dc9c7
maithuyquynh051821 <maithuyquynh051821@gmail.com>
2023-02-11 10:52:41 +0700

    add pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,23 @@
 class PagesController < ApplicationController
   def home
+    @post = Post.new
+    @posts = Post.all.order(created_at: :desc)
+    @nicknameList = []
+    @thumbnailList = []
+    @posts.each do |post|
+      name = User.find(post.user_id).name
+      avatar = User.find(post.user_id).avatar_thumbnail
+      @nicknameList << name
+      @thumbnailList << avatar
+    end
+  end
+
+  def search
+    @parameters = params[:query].downcase
+    if !@parameters.blank?
+      @search_user = User.all.where("id <> #{current_user.id} and lower(name) LIKE '%#{@parameters}%'")
+    else
+      @search_user = []
+    end
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
+
   # GET /posts or /posts.json
   def index
     @posts = Post.all
@@ -12,6 +13,7 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
+    @parent_id = params[:post]
     @post = Post.new
   end
 
@@ -25,7 +27,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to root_path, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
