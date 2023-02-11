c09dc6755bb4e95a98158806372453156a32c4db
Kimbaoksqp123 <kimbaoksqp123>
2023-02-11 12:18:33 +0700

    add users controllers

--- app/models/room.rb
+++ app/models/room.rb
@@ -21,6 +21,7 @@ class Room < ApplicationRecord
 
   def participant?(room, user)
     room.participants.where(user: user).exists?
+
     Participant.where(user_id: user.id, room_id: room.id).exists?
   end
 end
